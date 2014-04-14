88857679fd2ac6a590ad42c80336dd0cae0464c1
Alfredo López Moltó <xgalen@gmail.com>
2014-04-14 23:56:41 +0200

    Tests and inverse function

--- crypto.js
+++ crypto.js
@@ -1,10 +1,33 @@
+var letters = "acdegilmnoprstuw",
+    H = 7;
 var hash = function (s) {
-        var h = 7,
-            i, 
-            letters = "acdegilmnoprstuw";
+        var i, h = H;
         for (i = 0; i < s.length; i++) {
             h = h * 37 + letters.indexOf(s[i]);
         }
         return h;
     };
+
+function isInt(n) {
+    return n % 1 === 0;
+}
+
+function iterate(currentH, i) {
+    var h = (currentH - i) / 37;
+    if (isInt(h)) {
+        if (h > H) {
+            return iterate(h, letters.length - 1) + letters[i];
+        } else if (h == H) {
+            return letters[i];
+        } else { // h < H
+            return "";
+        }
+    } else {
+        return iterate(currentH, --i);
+    }
+}
+var inverse = function (h) {
+        return iterate(h, letters.length - 1);
+    };
 exports.hash = hash;
+exports.inverse = inverse;
--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 var crypto = require('./crypto'),
     assert = require('assert');
 describe('#hash()', function () {
+    it('should return 267 when the string param is "n"', function () {
+        assert.equal(267, crypto.hash("n"));
+    });
+    it('should return 9892 when the string param is "nt"', function () {
+        assert.equal(9892, crypto.hash("nt"));
+    });
+    it('should return 366007 when the string param is "nte"', function () {
+        assert.equal(366007, crypto.hash("nte"));
+    });
     it('should return 680131659347 when the string param is "leepadg"', function () {
         assert.equal(680131659347, crypto.hash("leepadg"));
+    });
+});
+describe('#reverse()', function () {
+    it('should return "n" when the hash param is 267', function () {
+        assert.equal("n", crypto.inverse(267));
+    });
+    it('should return "nt" when the hash param is 9892', function () {
+        assert.equal("nt", crypto.inverse(9892));
+    });
+    it('should return "nte" when the hash param is 366007', function () {
+        assert.equal("nte", crypto.inverse(366007));
+    });
+    it('should return "leepadg" when the hash param is 680131659347', function () {
+        assert.equal("leepadg", crypto.inverse(680131659347));
     });
 });
